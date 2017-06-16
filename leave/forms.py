c570bd1d635c7f2065c805d6b512b4f8c9b71d16
mokgadi <rasekgalam@gmail.com>
2017-06-16 20:04:00 +0200

    New Employee Dashboard styling

--- leave/forms.py
+++ leave/forms.py
@@ -3,14 +3,14 @@ from functools import partial
 DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 class LogLeaveForm(forms.Form):
-    startdate = forms.DateField( widget=DateInput())
-    enddate=forms.DateField(widget=DateInput() )
+    startdate = forms.DateField( widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
+    enddate=forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
 
 class CreateEmployeeForm(forms.Form):
-    username=forms.CharField(max_length=30,widget=forms.TextInput())
-    firstname=forms.CharField(widget=forms.TextInput())
-    lastname=forms.CharField(widget=forms.TextInput())
-    email=forms.CharField(widget=forms.EmailInput())
-    password=forms.CharField(widget=forms.PasswordInput())
-    startdate=forms.DateField( widget=DateInput())
+    username=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}))
+    firstname=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}))
+    lastname=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}))
+    email=forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control', 'type': 'email'}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'pasword'}))
+    startdate = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
 
