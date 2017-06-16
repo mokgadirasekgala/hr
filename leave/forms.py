8256c0f2a35874382651e86a5f410872b5c7641f
mokgadi <rasekgalam@gmail.com>
2017-06-16 13:16:25 +0200

    create new employee for

--- leave/forms.py
+++ leave/forms.py
@@ -6,3 +6,11 @@ class LogLeaveForm(forms.Form):
     startdate = forms.DateField( widget=DateInput())
     enddate=forms.DateField(widget=DateInput() )
 
+class CreateEmployeeForm(forms.Form):
+    username=forms.CharField(max_length=30,widget=forms.TextInput())
+    firstname=forms.CharField(widget=forms.TextInput())
+    lastname=forms.CharField(widget=forms.TextInput())
+    email=forms.CharField(widget=forms.EmailInput())
+    password=forms.CharField(widget=forms.PasswordInput())
+    startdate=forms.DateField( widget=DateInput())
+
--- leave/views.py
+++ leave/views.py
@@ -2,24 +2,30 @@ from leave.models import Leave, Employee
 from django.shortcuts import render
 from django.shortcuts import redirect
 import datetime
-from .forms import LogLeaveForm
+from .forms import LogLeaveForm, CreateEmployeeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .datehelpers import isValidLeavePrediod
 
 def create_employee(request): #for hr admin
+    form=CreateEmployeeForm(request.POST)
     if request.method == 'POST':
-        username = request.POST['username']
-        password=request.POST['password']
-        email=request.POST['email']
-        firstname=request.POST['firstname']
-        lastname=request.POST['lastname']
-        emp = Employee.objects.create_user(username=username, password=password, email=email, first_name=firstname,last_name=lastname, start_date=datetime.date(2017,1,1))
-        messages.success(request, "New Employee Created by Admin. Let the Employee login")
-        return redirect('login')
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            email=form.cleaned_data['email']
+            firstname=form.cleaned_data['firstname']
+            lastname=form.cleaned_data['lastname']
+            startdate=form.cleaned_data['startdate']
+            emp = Employee.objects.create_user(username=username, password=password, email=email, first_name=firstname,last_name=lastname, start_date=startdate)
+            messages.success(request, "New Employee Created by Admin. Let the Employee login")
+            return redirect('login')
+        else:
+            messages.error(request, "Something wrong with the details entered, try again ")
+            return redirect('/newemployee')
     else:
-        return render(request, 'leave/newemployee.html')
+        return render(request, 'leave/newemployee.html',{'form':form})
 
 
 @login_required(login_url="login/")
